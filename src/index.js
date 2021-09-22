c12ca1bd7a016149d02dbd442891ef4885406074
Bijay Tamang <lazon11@outlook.com>
2021-09-21 21:13:03 -0400

    firebase setup

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { GlobalStyles } from "./global-styles";
 import App from "./App";
+import { FirebaseContext } from "./context/firebase";
+
+const config = {
+  apiKey: "AIzaSyBb11OEfvm-71xeHKwhatDF5ZxVLxKpz4Q",
+  authDomain: "bijay-netflix.firebaseapp.com",
+  projectId: "bijay-netflix",
+  storageBucket: "bijay-netflix.appspot.com",
+  messagingSenderId: "247503255225",
+  appId: "1:247503255225:web:9a60770271b0fc43590d68",
+};
+
+const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
