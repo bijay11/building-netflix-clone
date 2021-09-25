e1dd165baebd2c97f4b31e71c8064a44270260b4
Bijay Tamang <lazon11@outlook.com>
2021-09-25 10:44:16 -0400

    Authentication Validated with firebase

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,21 +1,25 @@
 import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
-import { HeaderContainer } from "../containers/header";
+import { FirebaseContext } from "../context/firebase";
 import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
 import { Form } from "../components";
 import * as ROUTES from "../constants/routes";
-import { FirebaseContext } from "../context/firebase";
 
 export default function Signup() {
   const history = useHistory();
   const { firebase } = useContext(FirebaseContext);
+
   const [firstName, setFirstName] = useState("");
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleSignup = (e) => {
-    e.preventDefault();
+  const isInvalid = firstName === "" || password === "" || emailAddress === "";
+
+  const handleSignup = (event) => {
+    event.preventDefault();
+
     firebase
       .auth()
       .createUserWithEmailAndPassword(emailAddress, password)
@@ -23,54 +27,55 @@ export default function Signup() {
         result.user
           .updateProfile({
             displayName: firstName,
-            photoUrl: Math.floor(Math.random() * 5) + 1,
+            photoURL: Math.floor(Math.random() * 5) + 1,
           })
           .then(() => {
-            setEmailAddress("");
-            setPassword("");
-            setError("");
             history.push(ROUTES.BROWSE);
           })
       )
-      .catch((error) => setError(error.message));
+      .catch((error) => {
+        setEmailAddress("");
+        setPassword("");
+        setError(error.message);
+      });
   };
+
   return (
     <>
       <HeaderContainer>
         <Form>
           <Form.Title>Sign Up</Form.Title>
           {error && <Form.Error>{error}</Form.Error>}
-          <Form.Base onSubmit={handleSignup} method="POST"></Form.Base>
-          <Form.Input
-            placeholder="First Name"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
 
-          <Form.Input
-            placeholder="Email Address"
-            value={emailAddress}
-            onChange={(e) => setEmailAddress(e.target.value)}
-          />
-          <Form.Input
-            type="password"
-            value={password}
-            placeholder="Password"
-            autoComplete="off"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Form.Submit
-            type="submit"
-            disabled={firstName && emailAddress && password ? false : true}
-          >
-            Sign Up
-          </Form.Submit>
-          <Form.Text>
-            Already a user? <Form.Link to={ROUTES.SIGN_IN}>Sign In</Form.Link>
-          </Form.Text>
-          <Form.TextSmall>
-            This page is protected by Google reCAPTCHA.
-          </Form.TextSmall>
+          <Form.Base onSubmit={handleSignup} method="POST">
+            <Form.Input
+              placeholder="First Name"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <Form.Input
+              placeholder="Email Address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              value={password}
+              autoComplete="off"
+              placeholder="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign Up
+            </Form.Submit>
+
+            <Form.Text>
+              Already a user? <Form.Link to="/signin">Sign up now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA.
+            </Form.TextSmall>
+          </Form.Base>
         </Form>
       </HeaderContainer>
       <FooterContainer />
