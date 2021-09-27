6680c8ee6fc68486cdc6fb588b712f1660f96bcf
Bijay Tamang <lazon11@outlook.com>
2021-09-27 16:37:58 -0400

    value changed

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -16,7 +16,7 @@ export function BrowseContainer({ slides }) {
   const { firebase } = useContext(FirebaseContext);
 
   const user = {
-    displayName: "Karl",
+    displayName: "Bijay",
     photoURL: "1",
   };
 
